e2ed3d7fea573e7a229a0b37de7df6ae562fd608
Maryna7 <m.kurman23@gmail.com>
2024-03-12 14:26:21 +0100

    add the base folder stucture

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import InputField from './components/InputField'
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+     <span className="heading">Taskify</span>
+     <InputField />
+    </div>
+  );
+}
+
+export default App;
